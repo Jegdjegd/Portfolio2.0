63eed306f97f7a1795bf1923b9c51c34d7afb817
Jhoann G <jhoannegd@gmail.com>
2024-11-22 18:31:30 +0100

    First load

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AcercaDeComponent } from "./components/acerca-de/acerca-de.component";
+import { StackComponent } from "./components/stack/stack.component";
+import { StorieComponent } from "./components/storie/storie.component";
+import { ExperienceComponent } from "./components/experience/experience.component";
+import { ClipboardModule } from '@angular/cdk/clipboard';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AcercaDeComponent, StackComponent, StorieComponent, ExperienceComponent,ClipboardModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'portfolio2.0';
+  title = 'Portafolio Jhoann G';
 }
---
+++ src/app/components/acerca-de/acerca-de.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import {ClipboardModule} from '@angular/cdk/clipboard';
+
+
+
+@Component({
+  selector: 'app-acerca-de',
+  standalone: true,
+  imports: [ClipboardModule],
+  templateUrl: './acerca-de.component.html',
+  styleUrl: './acerca-de.component.css'
+})
+export class AcercaDeComponent {
+
+  email = "jhoannegd@gmail.com";
+  
+  copyText(){
+    alert(`El email ${this.email} fue copiado exitosamente`)
+  }
+
+}
